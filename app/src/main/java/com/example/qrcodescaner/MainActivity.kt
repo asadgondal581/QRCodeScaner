d423936c49bd746dd0721e69b03c6060758ce248
asad.nazeer <asadgondal581@gail.com>
2022-03-31 12:31:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/qrcodescaner/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.qrcodescaner
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.qrcodescaner.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+        binding.scanBtn.setOnClickListener {
+
+            val scaner = IntentIntegrator(this)
+            scaner.initiateScan()
+
+
+        }
+
+    }
+
+    // Register the launcher and result handler
+    private val barcodeLauncher = registerForActivityResult(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText(this@MainActivity, "Cancelled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "Scanned: " + result.contents,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    // Launch
+    fun onButtonClick(view: View?) {
+        barcodeLauncher.launch(ScanOptions())
+    }
+}
